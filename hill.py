ef9661db07d0895984a142bcc2fa22d0daece092
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2013-09-17 21:44:20 +0200

    Fix % 26 matrix multiplication.

--- hill.py
+++ hill.py
@@ -7,35 +7,62 @@ import string
 encode = lambda y:[ord(x)-97 for x in y]
 decode = lambda y:"".join([chr(x + 97) for x in y])
 
+def matmult(a, b):
+    zip_b = b.T.tolist()
+    zip_a = a.tolist()
+    return [[sum((ele_a*ele_b) % 26 for ele_a, ele_b in zip(row_a, col_b)) % 26 for col_b in zip_b] for row_a in zip_a]
 # from <http://stackoverflow.com/questions/4287721/easiest-way-to-perform-modular-matrix-inversion-with-python>
-def generalizedEuclidianAlgorithm(a, b):
-    if b > a:
-        #print a, b
-        return generalizedEuclidianAlgorithm(b,a);
-    elif b == 0:
-        return (1, 0);
+# http://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm
+def egcd(a, b):
+    """
+    Return a triple (g, x, y), such that ax + by = g = gcd(a, b).
+
+    >>> egcd(3, 2)
+    (1, 1, -1)
+    >>> egcd(5, 10)
+    (5, 1, 0)
+    >>> egcd(1, 0)
+    (1, 1, 0)
+    """
+    if a == 0:
+        return (b, 0, 1)
     else:
-        #print a,b
-        (x, y) = generalizedEuclidianAlgorithm(b, a % b);
-        return (y, x - (a / b) * y)
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+def modinv(a, m):
+    """
+    >>> modinv(3, 5)
+    2
+    """
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise AttributeError("%d mod %d has not inverse" % (a, m))  # modular inverse does not exist
+    else:
+        return x % m
 
 def inversemodp(a, p):
     a = a % p
     if (a == 0):
         print "a is 0 mod p"
         return 0
-    (x,y) = generalizedEuclidianAlgorithm(p, a % p);
+    (x, y) = generalizedEuclidianAlgorithm(p, a % p);
     return y % p
 
 def identitymatrix(n):
     return [[long(x == y) for x in range(0, n)] for y in range(0, n)]
 
 def inversematrix(matrix, q):
+    """
+    >>> k = np.array([2, 1, 3, 4])
+    >>> k.shape = (2, 2)
+    >>> inversematrix(np.mat(k), 26)
+    """
     n = len(matrix)
-    A = np.matrix([[ matrix[j, i] for i in range(0,n)] for j in range(0, n)], dtype = long)
+    A = np.matrix(matrix)
     Ainv = np.matrix(identitymatrix(n), dtype = long)
     for i in range(0, n):
-        factor = inversemodp(A[i,i], q)
+        factor = modinv(A[i,i], q)
         A[i] = A[i] * factor % q
         Ainv[i] = Ainv[i] * factor % q
         for j in range(0, n):
@@ -78,9 +105,16 @@ if __name__ == "__main__":
     cleartext = encode(cleartext)
     key_size = int(sys.argv[3])
     key = makeKey(encode(sys.argv[2]), key_size)
+
+    print 'key:', key
+
+    reversekey = np.array(inversematrix(key, 26))
+
+    print 'inverse', decode(itertools.chain.from_iterable([reversekey[0], reversekey[1]]))
+
     ciphertext = []
     for piece in chunks(cleartext, key_size):
         a = np.mat(piece)
-        ciphertext.append(np.array(np.dot(a, key))[0])
+        ciphertext.append(np.array(matmult(a, key))[0])
 
     print decode(list(itertools.chain.from_iterable([x % 26 for x in ciphertext])))
